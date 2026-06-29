4d83cff3bb8f0721fd5b34ca7620e003d414a236
z1dch <ceejay.gavina@outlook.com>
2026-06-29 23:15:29 +0800

    Improving function codes

--- index.js
+++ index.js
@@ -3,53 +3,14 @@ let homeScoreEl = document.getElementById("home-score-el")
 let timeoutEl = document.getElementById("timeout-el")
 let quarterEl = document.getElementById("quarter-el")
 
-let homeScore = 0;
-let timeout = 0;
-let quarter = 0;
-
-
-function twoPoints() {
-    homeScore += 2
-    homeScoreEl.textContent = homeScore
+function updateCounter(element, change) {
+    let currentCount = Number(element.textContent);
+    currentCount += change;
+    element.textContent = currentCount;
 }
 
-function threePoints() {
-    homeScore += 3
-    homeScoreEl.textContent = homeScore
+function resetCounter(element, change) {
+    currentCount = change;
+    element.textContent = currentCount;
 }
 
-function onePoint() {
-    homeScore += 1
-    homeScoreEl.textContent = homeScore
-}
-
-// will insert if else in order that the zero will not turn into negative number
-function minusPoint() {
-    homeScore -= 1
-    homeScoreEl.textContent = homeScore
-}
-
-function resetPoints() {
-    homeScore = 0
-    homeScoreEl.textContent = homeScore
-}
-
-function plusButton() {
-    timeout += 1
-    timeoutEl.textContent = timeout
-}
-
-function minusButton() {
-    timeout -= 1
-    timeoutEl.textContent = timeout
-}
-
-function plusQuarter() {
-    quarter += 1
-    quarterEl.textContent = quarter
-}
-
-function minusQuarter() {
-    quarter -= 1
-    quarterEl.textContent = quarter
-}
